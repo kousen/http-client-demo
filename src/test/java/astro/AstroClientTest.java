694c44c78535bcc7190191a5271c72ff33fe1eb4
Ken Kousen <ken.kousen@kousenit.com>
2018-09-18 17:33:23 -0400

    Add java.util.logging.Logger

--- src/test/java/astro/AstroClientTest.java
+++ src/test/java/astro/AstroClientTest.java
@@ -4,21 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AstroClientTest {
+    private Logger logger = Logger.getLogger(AstroClientTest.class.getName());
+
     private AstroClient client = new AstroClient();
 
     @Test
     void getSync() throws IOException, InterruptedException {
         String response = client.getSync();
-        System.out.println(response);
+        logger.info(response);
     }
 
     @Test
     void getAsync() throws ExecutionException, InterruptedException {
         String response = client.getAsync();
-        System.out.println(response);
+        logger.info(response);
     }
 }
