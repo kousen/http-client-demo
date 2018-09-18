e2f84f37dc4cdbfd9c71eb2ea0b26b9100c6c250
Ken Kousen <ken.kousen@kousenit.com>
2018-09-18 18:18:08 -0400

    Use Gson to parse response and test results

--- src/test/java/astro/AstroClientTest.java
+++ src/test/java/astro/AstroClientTest.java
@@ -1,9 +1,11 @@
 package astro;
 
+import astro.json.Assignment;
+import astro.json.AstroResponse;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import java.time.Duration;
+import java.util.List;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,14 +16,43 @@ class AstroClientTest {
     private AstroClient client = new AstroClient();
 
     @Test
-    void getSync() throws IOException, InterruptedException {
-        String response = client.getSync();
-        logger.info(response);
+    void getSync() {
+        AstroResponse response = assertTimeoutPreemptively(
+                Duration.ofSeconds(2),
+                () -> client.getSync());
+
+        int num = response.getNumber();
+        List<Assignment> assignments = response.getPeople();
+
+        assertEquals("success", response.getMessage());
+        assertEquals(num, assignments.size());
+        assignments.forEach(assignment ->
+                                    assertAll(() -> assertTrue(assignment.getName().length() > 0),
+                                              () -> assertTrue(assignment.getCraft().length() > 0)));
+
+        logResponse(num, assignments);
     }
 
     @Test
-    void getAsync() throws ExecutionException, InterruptedException {
-        String response = client.getAsync();
-        logger.info(response);
+    void getAsync() {
+        AstroResponse response = assertTimeoutPreemptively(
+                Duration.ofSeconds(2),
+                () -> client.getAsync());
+
+        int num = response.getNumber();
+        List<Assignment> assignments = response.getPeople();
+
+        assertEquals("success", response.getMessage());
+        assertEquals(num, assignments.size());
+
+        logResponse(num, assignments);
+    }
+
+    private void logResponse(int num, List<Assignment> assignments) {
+        logger.info(String.format("There are %d people in space", num));
+        assignments.forEach(person -> logger.info(
+                () -> String.format("%s aboard %s",
+                                    person.getName(),
+                                    person.getCraft())));
     }
 }
